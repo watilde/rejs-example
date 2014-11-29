9c2033f9eec77b20799f778f5f43cfe20c0d4ae9
Daijiro Wachi <daijiro.wachi@gmail.com>
2014-11-30 02:21:08 +0900

    Fixes indent

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var rejs = require('gulp-rejs');
 
 gulp.task('rejs', function () {
-    return gulp.src('src/*.js')
-        .pipe(rejs('XmasPresent.js'))
-        .pipe(gulp.dest('dist'));
+  return gulp.src('src/*.js')
+    .pipe(rejs('XmasPresent.js'))
+    .pipe(gulp.dest('dist'));
 });
